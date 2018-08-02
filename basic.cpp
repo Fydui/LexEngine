543addd7b14663ad53b95fc12dda973cd4d34ac2
Fydui <gtx980sli@yahoo.com>
2018-08-02 23:55:35 +0800

    等比例缩放基本完成
    窗口可以等比例缩放了,窗口中的图像也会跟着缩放,但因为使用了计时器的原因, 拉动窗口的时候有几率缩放无效(弹弹弹

--- basic.cpp
+++ basic.cpp
@@ -1,13 +1,12 @@
 ﻿#include "basic.h"
 
 
-int W = 0, H = 0;
 
 Basic::Basic( QWidget *parent)
     : QWidget(parent)
 {
     setWindowTitle(titleName);
-    setBaseSize(deskWidth,deskHeight);
+    setBaseSize(windowWidth,windowHeight);
     view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);       //关闭横竖滚动栏
     view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);   
 }
@@ -20,11 +19,13 @@ Basic::Basic(type t, QWidget *parent)
     //scene.addPixmap(QPixmap("C:/My/code/MyObj/MengDie_Widget/resources/image/Lexington_Strap_bikini.png"));
     //scene.addText("Hello");
     scene.setParent(this);
-    view.resize(deskWidth,deskHeight );          //设置视图范围      
+    view.resize(windowWidth,windowHeight );          //设置视图范围      
     view.setScene(&scene);
     //setWindowFlags(Qt::FramelessWindowHint);
     //setWindowFlag(Qt::WindowTitleHint);
     
+    lastHeight = windowHeight;
+    lastWidth = windowWidth;
     nt = t;
     
 }
@@ -46,9 +47,7 @@ void Basic::getWindowSize()
     
     deskHeight = screenRect.height();
     deskWidth = screenRect.width();
-    
-    screenHeightDifference = screenRect.height() - screenWindowHeight;
-    screenWidthDifference = screenRect.width() - screenWindowWidth;
+        
     
 }
 
@@ -60,8 +59,6 @@ void Basic::setWindowSize(int w, int h)
     deskHeight = h;
     deskWidth = w;
     
-    screenHeightDifference = 0;
-    screenWidthDifference = 0;
     
 }
 
@@ -72,16 +69,16 @@ void Basic::setFixed(type s)
         deskHeight = screenWindowHeight;
     }
     else if(s == Fix){
-        deskHeight = defaultWindowHeight;
-        deskWidth = defaultWindowWidth;
-        setFixedSize(deskWidth,deskHeight);        //固定窗口大小
+        deskHeight = windowHeight;
+        deskWidth = windowWidth;
+        setFixedSize(windowHeight,windowWidth);        //固定窗口大小
     }
     else{
-        deskHeight = defaultWindowHeight;
-        deskWidth = defaultWindowWidth;
-        setMinimumHeight(deskHeight);
-        setMinimumWidth(deskWidth);
-        
+        deskHeight = windowHeight;
+        deskWidth = windowWidth;
+        //setMinimumHeight(windowHeight);
+        //setMinimumWidth(windowWidth);
+        /*
         //获取窗口长宽比例:
         int temp = 1;
         int x = (deskHeight > deskWidth) ? deskHeight : deskWidth;
@@ -90,9 +87,7 @@ void Basic::setFixed(type s)
             temp = x%y;
             x = y;
             y = temp;
-        }
-        WindowRatioH = deskHeight/temp; //获得约分比例
-        WindowRatioW = deskWidth/temp;
+        }*/
         
     }
 }
@@ -111,7 +106,6 @@ void Basic::removeItem(Item &i)
     i.~Item();
 }
 
-bool flag = 0;
 void Basic::resizeEvent(QResizeEvent *event)
 {
     /*qDebug() << judgeHeightRatio();
@@ -121,49 +115,42 @@ void Basic::resizeEvent(QResizeEvent *event)
     if(w == h == 0)
         return;//resize(event->oldSize());
     if(w != 0 && h != 0)*/
-    
-    
-    int W = width()+width()*0.05;   //假设一个比例
-    int H = height()+height()*0.03;
-    //MessageBox(NULL,TEXT("test"),TEXT("ohh"),MB_OK);
-    //return;    
-    //SetWindowPos(getWindowHWND(),HWND_NOTOPMOST,0,0,W,H,SWP_NOMOVE);
-    int w = judgeWidthRatio();
-    int h = judgeHeightRatio();
-    if(w == h == 0) //如果比例不相等
-        return;
-    else
-        resize(w,h); //比例相等就设置
-
-    //qDebug() << "resize";
-    
+    if(timerEventFlag){
+        killTimer(timerEventFlag);
+        timerEventFlag = 0;
+    }
+    timerEventFlag = startTimer(50);
+    return;
 }
 
-void Basic::mousePressEvent(QMouseEvent *event)
+void Basic::timerEvent(QTimerEvent *event)
 {
-    //if(event->buttons()==Qt::LeftButton)
-    qDebug() <<"press";
-    getWindowHWND();
-    //resize(tW,newH);
+    if(width() == windowWidth){
+        windowWidth = height()* baseSize().width()/baseSize().height();
+        windowHeight = height();        
+    }
+    else{
+         windowWidth = width();
+         windowHeight = width()*baseSize().height()/baseSize().width();         
+    }
+    killTimer(event->timerId());
+    timerEventFlag = 0;
+    resize(windowWidth,windowHeight);
+    view.resize(windowWidth,windowHeight);
+    view.scale(qreal(windowWidth)/lastWidth,qreal(windowWidth)/lastWidth);
+    view.centerOn(0,0);
+    lastHeight = height();
+    lastWidth = width();
     
 }
 
-void Basic::mouseReleaseEvent(QMouseEvent *event)
-{
-    //if(event->buttons()==Qt::LeftButton)
-    qDebug() << "release";
-    
-}
 
-void Basic::changeEvent(QEvent *event)
-{
-    if(event->type() == QEvent::MouseTrackingChange)
-        qDebug() << "change Event";
-}
 
+
+/*
 int Basic::judgeWidthRatio()
 {
-    int w = width();    //获取当前窗口的宽 qt的函数
+    int w = windowWidth();    //获取当前窗口的宽 qt的函数
     for(int i = 1; i< sqrt(w); i++){
         if(w%i == 0){
             if(i == WindowRatioW || w/i == WindowRatioW) //WindowRationW == 16
@@ -175,7 +162,7 @@ int Basic::judgeWidthRatio()
 
 int Basic::judgeHeightRatio()
 {
-    int h = height();   //获取当前窗口的高 qt的函数
+    int h = windowHeight();   //获取当前窗口的高 qt的函数
     for(int i = 1; i< sqrt(h); i++){
         if(h%i == 0){
             if(i == WindowRatioH || h/i == WindowRatioH) //WindowRationH == 9
--- basic.h
+++ basic.h
@@ -5,12 +5,7 @@
 #include <QDesktopWidget>
 #include <QPixmap>
 #include <QDebug>
-#include <QResizeEvent>
-#include <QMouseEvent>
-#include <QEvent>
-#include <windows.h>
-#include <QEvent>
-#include <QByteArray>
+#include <QTimer>
 #include "item.h"
 enum type{Fix, Android, Flow};
 
@@ -20,8 +15,8 @@ class Basic : public QWidget
     Q_OBJECT
     
 public:
-    Basic(QWidget *parent = 0);
-    Basic(type t , QWidget *parent = 0);
+    Basic(QWidget *parent = nullptr);
+    Basic(type t , QWidget *parent = nullptr);
     ~Basic();
     void getWindowSize();   //获得窗口大小
     void setWindowSize(int w, int h);//设置窗口大小
@@ -30,32 +25,25 @@ public:
     void addItem(Item &i);
     void removeItem(Item &i);
     void resizeEvent(QResizeEvent * event);
-    void mousePressEvent(QMouseEvent* event);
-    void mouseReleaseEvent(QMouseEvent*event);
-    void changeEvent(QEvent* event);
+    void timerEvent(QTimerEvent* event);
     int judgeWidthRatio();  //判断当前窗口宽是否符合比例
     int judgeHeightRatio(); //判断当前窗口高是否符合比例
     //bool event(QEvent *event);
     
     QString titleName = "新项目";             //窗口标题的名字
-    int defaultWindowHeight = 720;        //窗口的大小
-    int defaultWindowWidth = 1280;   
+    int windowHeight= 720;        //窗口的大小
+    int windowWidth = 1280;   
+    int lastHeight; 
+    int lastWidth;
     int screenWindowHeight;          //可用桌面的大小
     int screenWindowWidth;
-    double deskWidth;                   //单块屏幕总大小
-    double deskHeight;
-    double screenHeightDifference;      //总大小与可用大小的差值
-    double screenWidthDifference;  
-    double widthZoomVar=1;              //默认长宽与当前长宽的比值 用于缩放图元
-    double heightZoomVar=1;
-    double zoomVar=1;
-    double WindowRatioH = 1;        //窗口宽高比
-    double WindowRatioW = 1;
+    int deskWidth;                   //单块屏幕总大小
+    int deskHeight;
+    int timerEventFlag= 0;
     
     QString prePath = ""; //安卓下的预路径 请在sdcard下自建MengDieOBJ梦蝶工程文件夹 将包放在该目录下
     
 private:
-    HWND getWindowHWND();
     type nt;    //当前模式
     View  view;
     Scene  scene;
